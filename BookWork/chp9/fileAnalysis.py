9717e672ca6cc07e48febd58fb9d31fa7b4ae17c
Claire-GetHub <claire@execor.com>
2025-01-27 12:21:45 -0700

    Update fileAnalysis.py

--- BookWork/chp9/fileAnalysis.py
+++ BookWork/chp9/fileAnalysis.py
@@ -1,12 +1,13 @@
 
-
+#opens 2 files
 def openFiles():
     while True:
         try:
             return open(input("file name 1: "), "r"), open(input("file name 2: "), "r")
         except FileNotFoundError:
             pass
-
+            
+#turn the file into a set. Each word being a value
 def fileToSet(file):
     list = []
     for line in file:
@@ -14,13 +15,20 @@ def fileToSet(file):
     return set(list)
 
 def main():
+    #get both files
     file1, file2 = openFiles()
+    #turn themm both to sets
     set1, set2 = fileToSet(file1), fileToSet(file2)
-    
+
+    #all unique words
     print(set1 ^ set2)
-    print(set1 | set2)
+    #all words in both files
+    print(set1 & set2)
+    #all words in file1 and not file2
     print(set1 - set2)
+    #all words in file2 and not file1
     print(set2 - set1)
+    #all words not in both files
     print(set1 ^ set2)
   
     
